4fcb9d9188d5c7c79d52164bf582b906d4162763
Ifrah Mohamed <ifrahfrah484@gmail.com>
2025-04-27 01:13:54 +0300

    Initial Commit

---
+++ SmartMeal/SmartMealApp.swift
@@ -0,0 +1,32 @@
+//
+//  SmartMealApp.swift
+//  SmartMeal
+//
+//  Created by Ifrah Mohamed on 27.04.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SmartMealApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
